91bca39660291d08560298669c3ebd3fe5354f5b
21Miya <mmshin@live.com>
2018-07-31 19:41:45 +0000

    Done.

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,30 @@
 require 'bundler'
+require './models/model'
 Bundler.require
 
-class MyApp < Sinatra::Base
-
+class Application < Sinatra::Base
+ 
   get '/' do
-    erb :index
+    "Hello, Class!"
   end
+  
+  get '/test' do
+  	"this is a test"
+  end
+  
+    
+  get '/input/:cat' do
+    @user_name = params[:cat]
+    "Hello #{@user_name}!"
+  end
+  
 
+  get '/form' do
+  	erb:index
+  end
+    
+  post '/food' do
+	"My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+ 
 end
