29b082229b6f07484d75477c7a7ba6fff60a90f0
21Miya <mmshin@live.com>
2018-07-31 19:06:51 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -4,7 +4,18 @@ Bundler.require
 class MyApp < Sinatra::Base
 
   get '/' do
+    "This is your home page"
+  end
+  
+  get '/itslit' do
     erb :index
+  end
+  
+  get '/hello/:band/:genre' do
+    @bandname = params[:band]
+    @genrename = params[:genre]
+    "Guess what?"
+    "#{@bandname} is the best #{@genrename} band!"
   end
 
 end
